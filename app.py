7745e46de73745aabd065580a153420584b7bc47
Christopher Budo <budocf@rose-hulman.edu>
2016-12-14 14:29:15 -0500

    added code to control pins

--- app.py
+++ app.py
@@ -46,6 +46,33 @@ def pincontrol():
     # main.html and return it to the    +++user
     return render_template('main.html', **templateData)
 
+@app.route("/<changePin>/<action>")
+def action(changePin, action):
+   # Convert the pin from the URL into an integer:
+   changePin = int(changePin)
+   # Get the device name for the pin being changed:
+   deviceName = pins[changePin]['name']
+   # If the action part of the URL is "on," execute the code indented below:
+   if action == "on":
+      # Set the pin high:
+      GPIO.output(changePin, GPIO.HIGH)
+      # Save the status message to be passed into the template:
+      message = "Turned " + deviceName + " on."
+   if action == "off":
+      GPIO.output(changePin, GPIO.LOW)
+      message = "Turned " + deviceName + " off."
+
+   # For each pin, read the pin state and store it in the pins dictionary:
+   for pin in pins:
+      pins[pin]['state'] = GPIO.input(pin)
+
+   # Along with the pin dictionary, put the message into the template data dictionary:
+   templateData = {
+      'pins' : pins
+   }
+
+   return render_template('main.html', **templateData)
+
 def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.',1)[1] in ALLOWED_EXTENSIONS
